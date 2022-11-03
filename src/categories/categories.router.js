616b87901f07d3e1cd65a5033b76d28a832f1895
Jorge <jorge.luis-leones@hotmail.com>
2022-11-03 17:24:57 -0500

    repices project

---
+++ src/categories/categories.router.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const passport = require("passport");
+
+const categoryServices = require("./categories.services");
+const adminMiddleware = require("../middlewares/role.middleware");
+
+require("../middlewares/auth.middleware")(passport);
+
+router.route("/")
+  .get(categoryServices.getAllCategories)
+  .post(
+    passport.authenticate("jwt", { session: false }),
+    adminMiddleware,
+    categoryServices.postCategory
+  );
+
+router.route("/:id")
+  .get(categoryServices.getCategoryById)
+  .delete(
+    passport.authenticate("jwt", { session: false }),
+    adminMiddleware,
+    categoryServices.deleteCategory
+  ); 
+
+module.exports = router;
